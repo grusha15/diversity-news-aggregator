3cd396fe89d4b1073735a2fdf43638f5653bce00
grusha15 <96344267+grusha15@users.noreply.github.com>
2024-10-13 12:32:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ async function fetchNewsForAllTopics() {
   let allArticles = [];
 
   for (let topic of diversityTopics) {
-    const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(topic)}&apiKey=${API_KEY}`;
+    const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(topic)}&apiKey=e9c4146a69d0414d9677a6703134a2c1`;
+
 
     try {
       const response = await axios.get(url, {
